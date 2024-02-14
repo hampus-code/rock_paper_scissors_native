950682ce6dc6eabc3577d6df0716d71b1db9d290
hampus-code <hampusss96@gmail.com>
2024-02-14 18:07:02 +0100

    Added background color by installing and import Linear Gradient

--- rock-paper-scissors/App.js
+++ rock-paper-scissors/App.js
@@ -1,11 +1,39 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Appearance,
+} from "react-native";
 import Game from "./component/Game";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
 const App = () => {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+
+  const toggleColorScheme = () => {
+    const newColorScheme = colorScheme === "dark" ? "light" : "dark";
+    setColorScheme(newColorScheme);
+  };
+
+  const DARK_COLORS = ["#4c669f", "#3b5998", "#192f6a"];
+  const LIGHT_COLORS = ["#aa4b6b", "#6b6b83", "#3b8d99"];
+
   return (
-    <View style={styles.container}>
-      <Game></Game>
-    </View>
+    <LinearGradient
+      colors={colorScheme === "dark" ? LIGHT_COLORS : DARK_COLORS}
+      style={styles.container}
+    >
+      <TouchableOpacity onPress={toggleColorScheme} style={styles.button}>
+        <Text style={styles.text}>
+          Switch to {colorScheme === "dark" ? "Light" : "Dark"} Mode
+        </Text>
+      </TouchableOpacity>
+      <View>
+        <Game></Game>
+      </View>
+    </LinearGradient>
   );
 };
 
@@ -15,6 +43,16 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "center",
     paddingTop: 50,
+  },
+  button: {
+    borderWidth: 1,
+    backgroundColor: "coral",
+    borderRadius: 4,
+    padding: 10,
+    marginHorizontal: 10,
+  },
+  text: {
+    color: "white",
   },
 });
 
